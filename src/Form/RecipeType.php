c696a3ffdbdc473d95dea1ab5453b3e5643ffe76
Marty Kane <marty@pixotech.com>
2018-09-09 21:19:24 -0500

    WIP attempt to embed the Ingredient form in the recipe form

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,11 @@ class RecipeType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('instructions')
-        ;
+            ->add('ingredients', CollectionType::class, array(
+                'entry_type' => IngredientType::class,
+                'entry_options' => array('label' => false),
+            ))
+            ->add('instructions');
     }
 
     public function configureOptions(OptionsResolver $resolver)
